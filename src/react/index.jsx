69891030bb0cfb601b31611b37179c913b7231aa
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-14 21:24:51 -0500

    Update to react 18

--- src/react/index.jsx
+++ src/react/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ChatApp from './ChatApp';
+import { createRoot } from 'react-dom/client';
 
 export function renderChatApp(domElement) {
     // For React 16, we use ReactDOM.render(...)
-    ReactDOM.render(<ChatApp />, domElement);
+    // ReactDOM.render(<ChatApp />, domElement);
+    const root = createRoot(domElement);
+    root.render(<ChatApp />);
 }
 
 // Unmount the React component
