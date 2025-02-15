b27ac2d1a9130ef84865afdbc9e39a3e0327c497
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-14 21:07:52 -0500

    typing for       "../../addon/content/reactBundle": ["typings/reactBundle.d.ts"]

---
+++ typings/reactBundle.d.ts
@@ -0,0 +1,5 @@
+// @ts-ignore reactBundle.js is not a module
+declare module "../addon/content/reactBundle.js" {
+    export function renderChatApp(domElement: HTMLElement): void;
+    export function unmountChatApp(domElement: HTMLElement): void;
+}
