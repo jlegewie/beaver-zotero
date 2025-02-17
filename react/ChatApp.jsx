7cc7a2b5e17d52e00ea7c4c9ce9becd24a736c48
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-17 14:03:20 -0500

    Initial message list

--- react/ChatApp.jsx
+++ react/ChatApp.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import UserMessageDisplay from "./components/UserMessageDisplay.tsx"
 import Header from "./components/Header.tsx"
-import { userAttachmentsAtom } from './atoms/messages';
-import { useSetAtom } from 'jotai';
+import { userAttachmentsAtom, messagesAtom } from './atoms/messages';
+import { useSetAtom, useAtomValue } from 'jotai';
 
 const ChatApp = () => {
     const inputRef = useRef(null);
     const setUserAttachments = useSetAtom(userAttachmentsAtom);
+    const messages = useAtomValue(messagesAtom);
     
     // Subscribe to events from Zotero
     useEffect(() => {
@@ -42,15 +43,40 @@ const ChatApp = () => {
     }, []);
     
     return (
-        <div className="chat-container px-3 py-2">
+        <div className="flex-col px-3 py-2">
             {/* Header */}
             <Header />
             
             {/* Chat Input */}
-            <UserMessageDisplay
-                inputRef={inputRef}
-                editing={false}
-            />
+            {messages.length === 0 && (
+                <UserMessageDisplay
+                    inputRef={inputRef}
+                    editing={true}
+                />
+            )}
+            
+            {/* Chat Messages */}
+            {messages.length > 0 && (
+                <div>
+                    {messages.map((message, index) => (
+                        <div key={index}>
+                            {message.role === 'user' && (
+                                <UserMessageDisplay
+                                    inputRef={inputRef}
+                                    editing={false}
+                                    message={message}
+                                />
+                            )}
+                        </div>
+                    ))}
+
+                    {/* Chat Input */}
+                    <UserMessageDisplay
+                        inputRef={inputRef}
+                        editing={true}
+                    />
+                </div>
+            )}
         </div>
     );
 };
