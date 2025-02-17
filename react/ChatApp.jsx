933d3287a728cc45dfdc1aab54432acae28e9239
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-17 17:20:56 -0500

    Layout of main chat panel

--- react/ChatApp.jsx
+++ react/ChatApp.jsx
@@ -40,41 +40,44 @@ const ChatApp = () => {
             eventBus.removeEventListener('focusChatInput', handleFocus);
             eventBus.removeEventListener('itemSelected', handleItemSelected);
         };
-    }, []);
+    }, [setUserAttachments]);
     
     return (
-        <div className="flex-col px-3 py-2">
+        <div className="flex-1 flex flex-col px-3 pt-2 pb-3">
             {/* Header */}
             <Header />
-            
-            {/* Chat Input */}
+
             {messages.length === 0 && (
                 <UserMessageDisplay
                     inputRef={inputRef}
                     editing={true}
                 />
             )}
-            
-            {/* Chat Messages */}
-            {messages.length > 0 && (
-                <div>
-                    {messages.map((message, index) => (
-                        <div key={index}>
-                            {message.role === 'user' && (
-                                <UserMessageDisplay
-                                    inputRef={inputRef}
-                                    editing={false}
-                                    message={message}
-                                />
-                            )}
-                        </div>
-                    ))}
 
-                    {/* Chat Input */}
-                    <UserMessageDisplay
-                        inputRef={inputRef}
-                        editing={true}
-                    />
+            {messages.length > 0 && (
+                <div className="flex flex-col flex-1 justify-between">
+                    {/* Scrollable messages area */}
+                    <div className="flex flex-col gap-2 flex-1 overflow-auto">
+                        {messages.map((message, index) => (
+                            <div key={index}>
+                                {message.role === 'user' && (
+                                    <UserMessageDisplay
+                                        inputRef={inputRef}
+                                        editing={false}
+                                        message={message}
+                                    />
+                                )}
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* Footer-like area for the new user prompt */}
+                    <div className="mt-2">
+                        <UserMessageDisplay
+                            inputRef={inputRef}
+                            editing={true}
+                        />
+                    </div>
                 </div>
             )}
         </div>
