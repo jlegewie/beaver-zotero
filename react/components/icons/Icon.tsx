dd1e80c2c4e496bc287eb6c6da592af91a0fb671
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-17 00:20:24 -0500

    Fix Icon component

--- react/components/icons/Icon.tsx
+++ react/components/icons/Icon.tsx
@@ -2,31 +2,34 @@ import React from 'react';
 
 interface IconProps {
     icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-    size?: number;
+    size?: number | string;
     color?: string;
     className?: string;
 }
 
 const Icon: React.FC<IconProps> = ({ 
     icon: IconComponent, 
-    size = 24, 
+    size = '1em',  // Default to 1em to scale with text
     color = 'currentColor',
     className = '',
     ...props 
 }) => {
+    // Remove the wrapper div and directly render the SVG
     return (
-        <div 
-            className={`inline-flex items-center justify-center ${className}`}
-            style={{ width: size, height: size }}
-        >
-            <IconComponent
-                width={size}
-                height={size}
-                color={color}
-                {...props}
-            />
-        </div>
+        <IconComponent
+            width={size}
+            height={size}
+            color={color}
+            className={`inline-block align-middle ${className}`}
+            style={{ 
+                // Fix vertical alignment
+                // transform: 'translateY(0.125em)',
+                // Ensure consistent sizing
+                flexShrink: 0
+            }}
+            {...props}
+        />
     );
 };
 
-export default Icon; 
+export default Icon;
