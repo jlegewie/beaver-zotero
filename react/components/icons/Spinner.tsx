82c538b892a0241488c6ee1d54c840509a3d4e83
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-20 17:59:49 -0500

    Update spinner

--- react/components/icons/Spinner.tsx
+++ react/components/icons/Spinner.tsx
@@ -1,32 +1,48 @@
 import React from 'react';
 
-const Spinner: React.FC<{ size?: number; className?: string }> = ({ 
-    size = 16, 
-    className = '' 
-}) => (
-    <svg
-        className={`animate-spin ${className}`}
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-    >
-        <circle 
-            className="opacity-25" 
-            cx="12" 
-            cy="12" 
-            r="10" 
-            stroke="currentColor" 
-            strokeWidth="4" 
-            fill="none" 
-        />
-        <path 
-            className="opacity-75" 
-            fill="currentColor" 
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" 
-        />
-    </svg>
-);
-
-export default Spinner;
+const Spinner = ({ size = 16, className = '' }) => {
+    const spinnerStyle = {
+        animation: 'spin 1s linear infinite',
+    };
+    
+    return (
+        <svg
+            style={spinnerStyle}
+            className={className}
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+        >
+            <style>
+                {`
+                @keyframes spin {
+                    from {
+                    transform: rotate(0deg);
+                    }
+                    to {
+                    transform: rotate(360deg);
+                    }
+                }
+                `}
+            </style>
+            <circle
+                style={{ opacity: 0.25 }}
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+                fill="none"
+            />
+            <path
+                style={{ opacity: 0.75 }}
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            />
+        </svg>
+        );
+    };
+    
+    export default Spinner;
