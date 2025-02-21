2c8b9789175d28bb8f5f77c3e97dd8384d7dbfbe
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-21 15:38:31 -0500

    Refactor ReaderSidebar to support Zotero tab selection

--- react/components/ReaderSidebar.tsx
+++ react/components/ReaderSidebar.tsx
@@ -3,13 +3,20 @@ import { useAtom } from "jotai";
 import AiSidebar from "../AiSidebar";
 import { isReaderSidebarVisibleAtom } from "../atoms/ui";
 import { useVisibility } from '../hooks/useVisibility';
+import { useWatchItemPaneCollapse } from '../hooks/useWatchItemPaneCollapse';
+import { useZoteroTabSelection } from '../hooks/useZoteroTabSelection';
+
+// ReaderSidebarContent handles library-specific features
+const ReaderSidebarContent = () => {
+    // useWatchItemPaneCollapse("reader");
+    useZoteroTabSelection();
+    return <AiSidebar location="reader" />;
+}
 
 const ReaderSidebar = () => {
     const [isVisible] = useAtom(isReaderSidebarVisibleAtom);
-    
     useVisibility('reader');
-
-    return isVisible ? <AiSidebar location="reader" /> : null;
+    return isVisible ? <ReaderSidebarContent /> : null;
 }
 
 export default ReaderSidebar; 
