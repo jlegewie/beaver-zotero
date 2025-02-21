6758da94d406c2dc6c01df9578f40dc6d7249308
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-21 14:55:31 -0500

    Shared store for consistency between instances of chat panel

--- react/index.tsx
+++ react/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'jotai';
+import { Provider, createStore } from 'jotai';
 import LibrarySidebar from './components/LibrarySidebar';
 import ReaderSidebar from './components/ReaderSidebar';
+
+// Create a shared store instance
+const store = createStore();
 
 export function renderAiSidebar(domElement: HTMLElement, location: 'library' | 'reader') {
     const root = createRoot(domElement);
     const Component = location === 'library' ? LibrarySidebar : ReaderSidebar;
     
     root.render(
-        <Provider>
+        <Provider store={store}>
             <Component />
         </Provider>
     );
