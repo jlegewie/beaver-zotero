fd3243b92d5c8188f4bb7c2434ef16699d07966e
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-22 15:04:37 -0500

    Update ui atoms

--- react/atoms/ui.ts
+++ react/atoms/ui.ts
@@ -1,7 +1,4 @@
 import { atom } from 'jotai';
 
-export const isReaderSidebarVisibleAtom = atom(false);
-export const isLibrarySidebarVisibleAtom = atom(false);
-
-
+export const isSidebarVisibleAtom = atom(false);
 export const isLibraryTabAtom = atom(false);
