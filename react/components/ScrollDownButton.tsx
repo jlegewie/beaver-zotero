7758a46a01a2f7748f9ec570af98a508452b1258
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-24 09:05:16 -0500

    Add ScrollDownButton component

---
+++ react/components/ScrollDownButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Icon, ArrowDown } from './icons';
+
+interface ScrollDownButtonProps extends React.HTMLProps<HTMLButtonElement> {
+    onClick: () => void;
+}
+
+export const ScrollDownButton: React.FC<ScrollDownButtonProps> = ({
+    onClick,
+    className,
+    style
+}) => {
+    return (
+        // <div className="absolute -top-4 left-1/2 -translate-x-1/2 -translate-y-full">
+        <div className="absolute left-1/2 -translate-x-1/2">
+            <button
+                onClick={onClick}
+                className={`scroll-down-button ${className || ''}`}
+                style={style}
+            >
+                <Icon icon={ArrowDown} />
+            </button>
+        </div>
+    );
+};
