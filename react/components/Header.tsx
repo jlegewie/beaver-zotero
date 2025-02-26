6083185f991edd5bd2e8faacb50556ac95e6745d
Joscha Legewie <joscha.legewie@gmail.com>
2025-02-26 14:25:28 -0500

    Update Header with IconButton

--- react/components/Header.tsx
+++ react/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Icon, CancelIcon, ClockIcon, PlusSignIcon, Settings02Icon } from './icons';
+import { CancelIcon, ClockIcon, PlusSignIcon } from './icons';
 import { triggerToggleChat } from '../../src/ui/toggleChat';
 import { messagesAtom } from '../atoms/messages';
 import { resetResourcesAtom, updateResourcesFromZoteroSelectionAtom } from '../atoms/resources';
-
 import { useSetAtom } from 'jotai';
+import IconButton from './IconButton';
 
 interface HeaderProps {
     onClose?: () => void;
@@ -24,26 +24,29 @@ const Header: React.FC<HeaderProps> = ({ onClose }) => {
     return (
         <div id="beaver-header" className="flex flex-row p-3 pb-2">
             <div className="flex-1">
-                <button
-                    className="icon-button scale-14"
+                <IconButton
+                    icon={CancelIcon}
                     onClick={() => triggerToggleChat(Zotero.getMainWindow())}
-                >
-                    <Icon icon={CancelIcon} />
-                </button>
+                    className="scale-14"
+                    ariaLabel="Close chat"
+                />
             </div>
             <div className="flex gap-4">
                 {/* <button className="icon-button scale-14">
                     <Icon icon={Settings02Icon} />
                 </button> */}
-                <button className="icon-button scale-14">
-                    <Icon icon={ClockIcon} />
-                </button>
-                <button
+                <IconButton
+                    icon={ClockIcon}
+                    className="scale-14"
+                    onClick={() => console.log('History')}
+                    ariaLabel="Show chat history"
+                />
+                <IconButton
+                    icon={PlusSignIcon}
                     onClick={handleNewThread}
-                    className="icon-button scale-14"
-                >
-                    <Icon icon={PlusSignIcon} />
-                </button>
+                    className="scale-14"
+                    ariaLabel="New thread"
+                />
             </div>
         </div>
     );
