a9e1f552d4db7afcfc2d27304ebde0bcc0ca9962
Joscha Legewie <joscha.legewie@gmail.com>
2025-03-01 14:25:56 -0500

    Update arrow icons

--- react/components/icons/ArrowDownIcon.tsx
+++ react/components/icons/ArrowDownIcon.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-type ArrowDownProps = React.SVGProps<SVGSVGElement>;
+type ArrowDownIconProps = React.SVGProps<SVGSVGElement>;
 
-const ArrowDown: React.FC<ArrowDownProps> = (props) => (
+const ArrowDownIcon: React.FC<ArrowDownIconProps> = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"} {...props}>
         <path d="M18 9.00005C18 9.00005 13.5811 15 12 15C10.4188 15 6 9 6 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
 
-export default ArrowDown;
+export default ArrowDownIcon;
--- react/components/icons/ArrowUpIcon.tsx
+++ react/components/icons/ArrowUpIcon.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-type ArrowUpProps = React.SVGProps<SVGSVGElement>;
+type ArrowUpIconProps = React.SVGProps<SVGSVGElement>;
 
-const ArrowUp: React.FC<ArrowUpProps> = (props) => (
+const ArrowUpIcon: React.FC<ArrowUpIconProps> = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"} {...props}>
         <path d="M17.9998 15C17.9998 15 13.5809 9.00001 11.9998 9C10.4187 8.99999 5.99985 15 5.99985 15" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
 
-export default ArrowUp;
+export default ArrowUpIcon;
