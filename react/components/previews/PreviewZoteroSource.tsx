287dd6c19cb8d9f6d7c128008fee9b0505e9a818
Joscha Legewie <joscha.legewie@gmail.com>
2025-03-02 08:34:25 -0500

    Adjust max length of note content

--- react/components/previews/PreviewZoteroSource.tsx
+++ react/components/previews/PreviewZoteroSource.tsx
@@ -7,13 +7,15 @@ interface PreviewZoteroSourceProps {
     item: Zotero.Item;
 }
 
+const MAX_NOTE_CONTENT_LENGTH = 250;
+
 const PreviewZoteroSource: React.FC<PreviewZoteroSourceProps> = ({ source, item }) => {
     const formatContent = (item: Zotero.Item) => {
         if (item.isNote()) {
             const title = item.getNoteTitle();
             // @ts-ignore getNote exists
             const content = Zotero.Utilities.unescapeHTML(item.getNote());
-            return content.replace(title, '').trim().slice(0, 30) + '...';
+            return content.replace(title, '').trim().slice(0, MAX_NOTE_CONTENT_LENGTH) + '...';
         }
         if (item.isAttachment()) {
             // @ts-ignore getFilename exists
