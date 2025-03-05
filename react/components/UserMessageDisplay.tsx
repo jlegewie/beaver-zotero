a69c391b80fdf8f27f97db03d02d49d691b36d45
Joscha Legewie <joscha.legewie@gmail.com>
2025-03-05 11:22:27 -0500

    Add right-click "copy" menu to UserMessageDisplay

--- react/components/UserMessageDisplay.tsx
+++ react/components/UserMessageDisplay.tsx
@@ -3,6 +3,10 @@ import { useAtomValue } from 'jotai';
 import { SourceButton } from "./SourceButton";
 import { ChatMessage } from '../types/messages';
 import { isStreamingAtom, threadSourcesAtom } from '../atoms/threads';
+// @ts-ignore no idea why
+import { useRef } from 'react';
+import ContextMenu from './ContextMenu';
+import useSelectionContextMenu from '../hooks/useSelectionContextMenu';
 
 interface UserMessageDisplayProps {
     message: ChatMessage;
@@ -14,6 +18,15 @@ const UserMessageDisplay: React.FC<UserMessageDisplayProps> = ({
     const isStreaming = useAtomValue(isStreamingAtom);
     const threadSources = useAtomValue(threadSourcesAtom);
     const messageSources = threadSources.filter(r => r.messageId === message.id);
+    const contentRef = useRef<HTMLDivElement | null>(null);
+
+    const { 
+        isMenuOpen: isSelectionMenuOpen, 
+        menuPosition: selectionMenuPosition,
+        closeMenu: closeSelectionMenu,
+        handleContextMenu,
+        menuItems: selectionMenuItems
+    } = useSelectionContextMenu(contentRef);
 
     return (
         <div className="user-message-display">
@@ -32,10 +45,18 @@ const UserMessageDisplay: React.FC<UserMessageDisplayProps> = ({
             )}
 
             {/* Message content */}
-            <div className="-ml-1">
+            <div className="-ml-1 user-select-text" ref={contentRef} onContextMenu={handleContextMenu}>
                 {message.content}
             </div>
 
+            {/* Text selection context menu */}
+            <ContextMenu
+                menuItems={selectionMenuItems}
+                isOpen={isSelectionMenuOpen}
+                onClose={closeSelectionMenu}
+                position={selectionMenuPosition}
+                useFixedPosition={true}
+            />
         </div>
     );
 };
