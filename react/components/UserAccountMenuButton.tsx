4bd809aa7ac42af10a78d921337091e1810f698b
Joscha Legewie <joscha.legewie@gmail.com>
2025-03-19 13:18:50 -0400

    Add UserAccountMenuButton component

---
+++ react/components/UserAccountMenuButton.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import MenuButton from './MenuButton';
+import { MenuItem } from './ContextMenu';
+import { SettingsIcon, UserIcon, LogoutIcon } from './icons';
+import { supabase } from '../../src/services/supabaseClient';
+
+interface UserAccountMenuButtonProps {
+    className?: string;
+    ariaLabel?: string;
+}
+
+/**
+ * Button component that shows user account menu in a dropdown menu
+ */
+const UserAccountMenuButton: React.FC<UserAccountMenuButtonProps> = ({ 
+    className = '',
+    ariaLabel = 'User Account Menu'
+}) => {
+
+    // Create menu items from threads
+    const menuItems: MenuItem[] = [
+        {
+            label: "Settings",
+            onClick: () => console.log('manage account'),
+            icon: SettingsIcon,
+        },
+        {
+            label: "Manage account",
+            onClick: () => console.log('manage account'),
+            icon: UserIcon,
+        },
+        {
+            label: "Logout",
+            onClick: () => {
+                supabase.auth.signOut();
+            },
+            icon: LogoutIcon,
+        }
+    ];
+
+    return (
+        <MenuButton
+            menuItems={menuItems}
+            variant="ghost"
+            icon={UserIcon}
+            className={className}
+            ariaLabel={ariaLabel}
+            tooltipContent="User account and settings"
+        />
+    );
+};
+
+export default UserAccountMenuButton; 
