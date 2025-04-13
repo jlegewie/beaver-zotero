ca812966f97523f0938072af64f1f0151bc7c9c1
Joscha Legewie <joscha.legewie@gmail.com>
2025-04-13 14:03:20 -0400

    Update warning message

--- react/components/ErrorWarningDisplay.tsx
+++ react/components/ErrorWarningDisplay.tsx
@@ -62,30 +62,44 @@ export const ErrorDisplay: React.FC<{ errorType: string }> = ({ errorType }) => 
 
 const getWarning = (type: string) => {
     switch (type) {
-        case 'app_key_limit_exceeded':
-            return "You have exceeded your monthly chat limit. Consider adding your own API key in settings.";
+        case 'user_key_failed_unexpected':
+            return "Your API key failed because of an unexpected error. Please check your API key in settings. Using our application key as a fallback.";
+        case 'user_key_rate_limit_exceeded':
+            return "Your API key failed because of a rate limit. Please check your API key in settings. Using our application key as a fallback.";
+        case 'user_key_failed':
+            return "Unable to authenticate with your API key. Please confirm it is correct. Using our application key as a fallback.";
+        case 'missing_attachments':
+            return "Unable to process attachment XXX. Please check the attachment and try again.";
         default:
-            return "Test warning message.";
+            return "Unexpected error using your API key. Using our application key as a fallback.";
     }
-}
-
+};
 
 export const WarningDisplay: React.FC<{ message: Warning }> = ({ message }) => {
     const setIsPreferencePageVisible = useSetAtom(isPreferencePageVisibleAtom);
+
+    const showSettingsIcon = message.type === 'user_key_failed_unexpected' || message.type === 'user_key_rate_limit_exceeded' || message.type === 'user_key_failed';
     
     return (
         <div className="flex flex-col gap-0">
-            <div className="font-color-yellow px-2 py-3 flex flex-row gap-4 items-center">
-                <Icon icon={AlertIcon} className="scale-12"/>
-                <span>{getWarning(message.type)}</span>
-                {/* {message.showSettingsButton &&
-                    <div className="flex flex-1 flex-row">
-                        <IconButton variant="outline" icon={KeyIcon} onClick={() => {
-                            setIsPreferencePageVisible(true);
-                        }} />
+            <div className="font-color-yellow px-2 py-3 flex flex-row gap-4 items-start">
+                <Icon icon={AlertIcon} className="scale-12 mt-1"/>
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-1 items-center">
+                        <div>Warning</div>
+                        <div className="flex-1"/>
+                        {showSettingsIcon &&
+                            <Button variant="outline" rightIcon={KeyIcon} className="mr-4" onClick={() => {
+                                setIsPreferencePageVisible(true);
+                            }}>
+                                API Key
+                            </Button>
+                        }
                     </div>
-                } */}
+                    <div className="text-sm">{getWarning(message.type)}</div>
+                </div>
             </div>
+            
         </div>
     );
 };
