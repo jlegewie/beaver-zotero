fd306fb4d2c918d955d5eefafd6510f014ede114
Joscha Legewie <joscha.legewie@gmail.com>
2025-04-15 12:23:10 -0400

    Add profile management state with Jotai atoms and fetchProfileAtom to fetch and set profile data

---
+++ react/atoms/profile.ts
@@ -0,0 +1,17 @@
+import { atom } from "jotai";
+import { ProfileWithPlanName } from "../types/profile";
+import { accountService } from "src/services/accountService";
+
+export const profileAtom = atom<ProfileWithPlanName | null>(null);
+
+export const fetchProfileAtom = atom(
+    null,
+    async (get, set) => {
+        try {
+            const profileFetched = await accountService.getProfile();
+            set(profileAtom, profileFetched);
+        } catch (error: any) {
+            Zotero.debug('Error fetching profile:', error, 3);
+        }
+    }
+);
