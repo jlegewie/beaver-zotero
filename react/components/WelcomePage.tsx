65934c7377b90a2113541fde7ab69a59f7a6b10b
Joscha Legewie <joscha.legewie@gmail.com>
2025-04-18 08:25:30 -0400

    Refactor WelcomePage component: Remove unused fetchProfile atom and related useEffect for profile fetching

--- react/components/WelcomePage.tsx
+++ react/components/WelcomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // @ts-ignore no idea
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from "./button";
 import FileStatusStats from "./FileStatusStats";
 import FileStatusDisplay from "./FileStatusDisplay";
@@ -14,7 +14,6 @@ import { getAppState } from "../utils/appState";
 import { isStreamingAtom } from '../atoms/threads';
 import { generateResponseAtom } from '../atoms/generateMessages';
 import { currentSourcesAtom } from "../atoms/input";
-import { fetchProfileWithPlanAtom } from "../atoms/profile";
 
 const getQuickPromptPreferences = (): QuickPrompt[] => {
     const quickPrompts: QuickPrompt[] = [];
@@ -93,15 +92,8 @@ const WelcomePage: React.FC = () => {
     const isStreaming = useAtomValue(isStreamingAtom);
     const currentSources = useAtomValue(currentSourcesAtom);
     const generateResponse = useSetAtom(generateResponseAtom);
-    const fetchProfile = useSetAtom(fetchProfileWithPlanAtom);
 
     // Realtime listening for file status updates
-    useFileStatus();
-
-    useEffect(() => {
-        console.log('Fetching profile');
-        fetchProfile();
-    }, []);
     useFileStatus();
 
     const handleQuickPrompt = async (
