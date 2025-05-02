98688d758c0d8752c9b6e807fcbedc9854a9b557
Joscha Legewie <joscha.legewie@gmail.com>
2025-05-02 13:59:18 -0400

    Add Zotero icons for annotations

--- react/components/icons/ZoteroIcon.tsx
+++ react/components/icons/ZoteroIcon.tsx
@@ -52,4 +52,10 @@ export const ZOTERO_ICONS = {
     PLUS: '16/universal/plus.svg',
     MINUS: '16/universal/minus.svg',
     SHOW_ITEM: '16/universal/show-item.svg',
+    ANNOTATE_AREA: '16/universal/annotate-area.svg',
+    ANNOTATE_HIGHLIGHT: '16/universal/annotate-highlight.svg',
+    ANNOTATE_NOTE: '16/universal/annotate-note.svg',
+    ANNOTATE_TEXT: '16/universal/annotate-text.svg',
+    ANNOTATE_UNDERLINE: '16/universal/annotate-underline.svg',
+    ANNOTATION: '16/universal/annotation.svg'
 } as const; 
