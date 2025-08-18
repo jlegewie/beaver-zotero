f84b1b93fd480ba6ba135c0f2d6e88ed2b521811
Joscha Legewie <joscha.legewie@gmail.com>
2025-08-17 21:34:27 -0400

    remove pages from allowed citation attributes

--- react/components/messages/MarkdownRenderer.tsx
+++ react/components/messages/MarkdownRenderer.tsx
@@ -13,7 +13,7 @@ const customSchema = deepmerge(defaultSchema, {
     tagNames: [...(defaultSchema.tagNames || []), 'citation'],
     attributes: {
         ...defaultSchema.attributes,
-        citation: ['id', 'cid', 'pids', 'pages', 'consecutive']
+        citation: ['id', 'cid', 'pids', 'consecutive']
     }
 });
 
