815e045e398a14938cf5c9b49b5efcd349f031e2
Joscha Legewie <joscha.legewie@gmail.com>
2025-09-04 10:35:00 -0400

    Update list of permitted attributes for citation tags

--- react/components/messages/MarkdownRenderer.tsx
+++ react/components/messages/MarkdownRenderer.tsx
@@ -13,7 +13,7 @@ const customSchema = deepmerge(defaultSchema, {
     tagNames: [...(defaultSchema.tagNames || []), 'citation'],
     attributes: {
         ...defaultSchema.attributes,
-        citation: ['id', 'cid', 'pids', 'consecutive']
+        citation: ['id', 'cid', 'sid', 'consecutive']
     }
 });
 
