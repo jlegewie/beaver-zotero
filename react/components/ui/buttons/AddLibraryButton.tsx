26b2627637aca780e6a63580ae95f7a8ad255228
Joscha Legewie <joscha.legewie@gmail.com>
2025-09-16 12:49:45 -0400

    AddLibraryButton: Only show search field if items about 5

--- react/components/ui/buttons/AddLibraryButton.tsx
+++ react/components/ui/buttons/AddLibraryButton.tsx
@@ -160,6 +160,7 @@ const AddLibraryButton: React.FC<AddLibraryButtonProps> = ({ disabled=false }) =
                 closeOnSelect={true}
                 searchQuery={searchQuery}
                 setSearchQuery={setSearchQuery}
+                minItemsForSearch={5}
             />
         </>
     );
