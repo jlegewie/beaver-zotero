63e17bbfe79b8de02c2cae03d4d819047b0f0c2b
Joscha Legewie <joscha.legewie@gmail.com>
2025-11-22 15:41:08 -0500

    feat(externalReferences): enhance ActionButtons with tooltips and refactor button layout for improved user experience

--- react/components/externalReferences/actionButtons.tsx
+++ react/components/externalReferences/actionButtons.tsx
@@ -12,6 +12,8 @@ import {
 } from '../../atoms/ui';
 import Button from '../ui/Button';
 import { ExternalReferenceResult } from '../../types/chat/apiTypes';
+import IconButton from '../ui/IconButton';
+import Tooltip from '../ui/Tooltip';
 
 interface ActionButtonsProps {
     item: ExternalReference | ExternalReferenceResult;
@@ -27,53 +29,70 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({
 
     return (
         <div className="display-flex flex-row items-center gap-3">
-            {showAbstractButton && (
+            <Tooltip content="Import to Zotero">
                 <Button
                     variant="surface-light"
-                    // icon={ArrowUpRightIcon}
+                    icon={DownloadIcon}
                     className="font-color-secondary truncate"
-                    onClick={() => {
-                        setSelectedReference(item);
-                        setIsDetailsVisible(true);
-                    }}
-                    disabled={!item.abstract}
+                    onClick={() => (item.publication_url || item.url) ? Zotero.launchURL(item.publication_url || item.url!) : undefined}
+                    disabled={!item.publication_url && !item.url}
                     style={{ padding: '1px 4px' }}
                 >
-                    Abstract
+                    Import
                 </Button>
-            )}                      
-            <Button
-                variant="surface-light"
-                icon={DownloadIcon}
-                className="font-color-secondary truncate"
-                onClick={() => (item.publication_url || item.url) ? Zotero.launchURL(item.publication_url || item.url!) : undefined}
-                disabled={!item.publication_url && !item.url}
-                style={{ padding: '1px 4px' }}
-            >
-                Import
-            </Button>
-            <Button
-                variant="surface-light"
-                icon={ArrowUpRightIcon}
-                className="font-color-secondary truncate"
-                onClick={() => (item.publication_url || item.url) ? Zotero.launchURL(item.url || item.publication_url!) : undefined}
-                disabled={!item.publication_url && !item.url}
-                style={{ padding: '1px 4px' }}
-            >
-                Website
-            </Button>
-            {item.open_access_url && (
+            </Tooltip>
+            {showAbstractButton && (
+                <Tooltip content="Open details">
+                    <Button
+                        variant="surface-light"
+                        // icon={ArrowUpRightIcon}
+                        className="font-color-secondary truncate"
+                        onClick={() => {
+                            setSelectedReference(item);
+                            setIsDetailsVisible(true);
+                        }}
+                        disabled={!item.abstract}
+                        style={{ padding: '1px 4px' }}
+                    >
+                        Details
+                    </Button>
+                </Tooltip>
+            )}
+            <Tooltip content="Open website">
                 <Button
                     variant="surface-light"
-                    icon={PdfIcon}
+                    icon={ArrowUpRightIcon}
                     className="font-color-secondary truncate"
-                    iconClassName="icon-11"
-                    onClick={() => item.open_access_url? Zotero.launchURL(item.open_access_url!) : undefined}
-                    disabled={!item.open_access_url}
+                    onClick={() => (item.publication_url || item.url) ? Zotero.launchURL(item.url || item.publication_url!) : undefined}
+                    disabled={!item.publication_url && !item.url}
                     style={{ padding: '1px 4px' }}
                 >
-                    PDF
-                </Button>  
+                    Web
+                </Button>
+            </Tooltip>
+            {/* <Tooltip content="Open website">
+                <IconButton
+                    variant="surface-light"
+                    icon={ArrowUpRightIcon}
+                    className="font-color-secondary truncate"
+                    onClick={() => (item.publication_url || item.url) ? Zotero.launchURL(item.url || item.publication_url!) : undefined}
+                    disabled={!item.publication_url && !item.url}
+                    style={{ padding: '3px 4px' }}
+                />
+            </Tooltip> */}
+            {item.open_access_url && (
+                <Tooltip content="Open PDF">
+                    <IconButton
+                        variant="surface-light"
+                        icon={PdfIcon}
+                        className="font-color-secondary truncate"
+                        iconClassName="icon-12"
+                        ariaLabel="Open PDF"
+                        onClick={() => item.open_access_url ? Zotero.launchURL(item.open_access_url!) : undefined}
+                        disabled={!item.open_access_url}
+                        style={{ padding: '3px 4px' }}
+                    />
+                </Tooltip>
             )}
             <div className="font-color-tertiary">Cited by {(item.citation_count || 0).toLocaleString()}</div>
         </div>
