705a6c77464655815bfe351d2738faef1cf00fad
Joscha Legewie <joscha.legewie@gmail.com>
2025-11-25 23:12:32 -0500

    feat(DocumentValidationIcon): add new SVG icon for document validation to enhance visual representation in the UI

--- react/components/icons/icons.tsx
+++ react/components/icons/icons.tsx
@@ -10,6 +10,7 @@ export { default as SettingsIcon } from './SettingsIcon';
 export { default as SettingsSolidIcon } from './SettingsSolidIcon';
 export { default as PdfIcon } from './PdfIcon';
 export { default as PinIcon } from './PinIcon';
+export { default as DocumentValidationIcon } from './DocumentValidationIcon';
 export { default as DownloadIcon } from './DownloadIcon';
 export { default as AttachmentIcon } from './AttachmentIcon';
 export { default as ArrowUpIcon } from './ArrowUpIcon';
