17c3cd15545b51c153a967c1efc7e0805c222291
Joscha Legewie <joscha.legewie@gmail.com>
2025-11-26 09:41:46 -0500

    fix(GlobalSearchIcon): spread props to SVG element for improved flexibility and customization

--- react/components/icons/GlobalSearchIcon.tsx
+++ react/components/icons/GlobalSearchIcon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type GlobalSearchIconProps = React.SVGProps<SVGSVGElement>;
 
 const GlobalSearchIcon: React.FC<GlobalSearchIconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none" {...props}>
         <path d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
         <path d="M20 5.69899C19.0653 5.76636 17.8681 6.12824 17.0379 7.20277C15.5385 9.14361 14.039 9.30556 13.0394 8.65861C11.5399 7.6882 12.8 6.11636 11.0401 5.26215C9.89313 4.70542 9.73321 3.19045 10.3716 2" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round"></path>
         <path d="M2 11C2.7625 11.6621 3.83046 12.2682 5.08874 12.2682C7.68843 12.2682 8.20837 12.7649 8.20837 14.7518C8.20837 16.7387 8.20837 16.7387 8.72831 18.2288C9.06651 19.1981 9.18472 20.1674 8.5106 21" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round"></path>
