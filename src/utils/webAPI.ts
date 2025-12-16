4e37d3ea32dc37439c84ffad18ff2377399d078a
Joscha Legewie <joscha.legewie@gmail.com>
2025-12-16 14:12:53 -0500

    fix: improve attachment server check in isAttachmentOnServer function
    - Update the `isAttachmentOnServer` function to return a boolean value based on the presence of `attachmentSyncedHash`, enhancing the clarity and reliability of the attachment availability check.
    - This change ensures a more consistent evaluation of whether an attachment is present on the server, improving overall sync accuracy.

--- src/utils/webAPI.ts
+++ src/utils/webAPI.ts
@@ -24,7 +24,7 @@ export function isAttachmentOnServer(item: Zotero.Item): boolean {
 	if (!item || !item.isStoredFileAttachment()) return false;
 	
 	// File is on server if it has a server hash
-	return item.attachmentSyncedHash !== null;
+	return Boolean(item.attachmentSyncedHash);
 }
 
 /**
