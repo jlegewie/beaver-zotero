1c9cfb541d4b2356c948317e35520d1ef40827bc
Joscha Legewie <joscha.legewie@gmail.com>
2026-04-06 21:39:27 -0400

    feat: update CreateNotePreview component to conditionally apply padding based on streaming state for improved layout

--- react/components/agentRuns/CreateNotePreview.tsx
+++ react/components/agentRuns/CreateNotePreview.tsx
@@ -24,7 +24,7 @@ export const CreateNotePreview: React.FC<CreateNotePreviewProps> = ({
 
     return (
         <div className="display-flex flex-col">
-            <div className="display-flex flex-col px-25 pt-2 gap-2">
+            <div className={`display-flex flex-col px-25 pt-2 gap-2 ${isStreaming ? 'pb-2' : ''}`}>
                 <div className="markdown note-body">
                     <MarkdownRenderer
                         content={trimmedContent || (isStreaming ? '_Generating..._' : '_No content yet._')}
